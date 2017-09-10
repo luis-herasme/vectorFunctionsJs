774073fa68e8f15b14f569341f370e28c47938c0
LuisHerasme <luis.alberto.herasme.cuevas@gmail.com>
2017-09-10 09:16:36 -0400

    Adding all the documentation
    - All the documentation was added
    - All test working and added

--- test/vector.js
+++ test/vector.js
@@ -4,21 +4,91 @@
 const expect = require('chai').expect
 const vector = require('../vector')
 
+describe('Given an angle', function () {
+  it('I should be able to convert that angle to degree', function () {
+    expect(180).deep.equal(vector.toDegree(Math.PI))
+  })
+
+  it('I should be able to convert that angle to radians', function () {
+    expect(Math.PI).deep.equal(vector.toRadian(180))
+  })
+})
+
 describe('Given an instance of the vector functions set', function () {
   it('I should be able to add 2 vectors', function () {
     expect([1, 10]).deep.equal(vector.add([0, 10], [1, 0]))
     expect([0, 5, 10]).deep.equal(vector.add([0, 3.5], [0, 1.5], [0, 0, 10]))
   })
+
   it('I should be able to subtract 2 vectors', function () {
     expect([9, 10]).deep.equal(vector.sub([10, 20], [1, 10]))
   })
+
   it('I should be able to scale a vectors', function () {
     expect([10, 10]).deep.equal(vector.mult([5, 5], 2))
   })
+
   it('I should be able to inverse a vectors', function () {
     expect([-5, -5]).deep.equal(vector.inverse([5, 5]))
   })
+
+  it('I should be able to normalize a vectors', function () {
+    expect([0.6, 0.8]).deep.equal(vector.normalize([3, 4]))
+  })
+
   it('I should be able to see the length of a vectors', function () {
     expect(5).deep.equal(vector.mag([3, 4]))
+  })
+
+  it('I should be able to limit the length of a vector', function () {
+    expect(4).deep.equal(vector.mag(vector.limit([3, 4], 4)))
+  })
+
+  it('I should be able to copy a vector', function () {
+    expect([3, 4]).deep.equal(vector.copy([3, 4]))
+  })
+
+  it('I should be able to get the dot product of 2 vectors', function () {
+    expect(200).deep.equal(vector.dot([10, 10], [10, 10]))
+  })
+
+  it('I should be able to get the angle of a vector', function () {
+    expect(Math.PI / 4).deep.equal(vector.angle([10, 10]))
+  })
+
+  it('I should be able to move a vector moveTowards another vector', function () {
+    expect([2, 0]).deep.equal(vector.moveTowards([0, 0], [5, 0], 2))
+  })
+
+  it('I should be able to see the distance between 2 vectors', function () {
+    expect(5).deep.equal(vector.distance([10, 10], [14, 13]))
+  })
+
+  it('I should be able to see the angle between 2 vectors', function () {
+    expect(Math.PI / 2).deep.equal(vector.angleBetween([0, 10], [10, 0]))
+  })
+
+  it('I should be able to set an angle to a vector', function () {
+    expect(Math.PI / 2).deep.equal(vector.angle(vector.setAngle([10, 10], Math.PI / 2)))
+  })
+
+  it('I should be able to add an angle to a vector', function () {
+    expect(Math.PI).deep.equal(vector.angle(vector.addAngle([0, 10], Math.PI / 2)))
+  })
+
+  it('I should be able use cross2d', function () {
+    expect(0).deep.equal(vector.cross2d([3, 4], [3, 4]))
+  })
+
+  it('I should be able to create a vector from direction and magnitude', function () {
+    expect([10, 0]).deep.equal(vector.angleMagnitude(0, 10))
+  })
+
+  it('I should be able to get the cross3d product of 2 3d vectors', function () {
+    expect([-5, 45, -35]).deep.equal(vector.cross3d([5, 6, 7], [10, 5, 5]))
+  })
+
+  it('I should be able to see the linearIntersect of 2 lines', function () {
+    expect([0.5, 0.5]).deep.equal(vector.linearIntersect([0, 0], [10, 0], [5, 5], [5, -5]))
   })
 })
--- vector.js
+++ vector.js
@@ -66,7 +66,7 @@ function inverse (vector) {
 function normalize (vector) {
   let r = mag(vector)
   if (isNaN(r) || r === 0) r = 1
-  return vector.mag((x) => x / r)
+  return vector.map((x) => x / r)
 }
 
 /**
@@ -117,9 +117,17 @@ function mag (vector) {
   return Math.sqrt(dot(vector, vector))
 }
 
-// Moves the first vector given to the second vector given
-function moveTowards (start, ends, speed = 1, stop = 1) {
-  let distance = sub(ends, start)
+/**
+ * Moves the first vector given to the second vector given
+ * with the given speed and wont move if the magnitude of the distance is
+ * smaller than the stop parameter.
+ * @param {array} start This is an array of components of a vector
+ * @param {array} end This is an array of components of a vector
+ * @param {number} speed This is the speed in wich the first vector will move towards the second
+ * @param {number} stop This is the distance in wich the start vector will not move to the end vector
+ */
+function moveTowards (start, end, speed = 1, stop = 1) {
+  let distance = sub(end, start)
   if (mag(distance) > stop) {
     distance = normalize(distance)
     distance = mult(distance, speed)
@@ -193,41 +201,22 @@ function angle (vector) {
   return result
 }
 
-function addAngle (vec, ang, piv) {
-  let vecR = copy(vec)
-
-  let s = Math.sin(ang)
-  let c = Math.cos(ang)
-
-  vecR = sub(vecR, piv)
-
-  let xnew = vecR[0] * c - vecR[1] * s
-  let ynew = vecR[0] * s + vecR[1] * c
-
-  vecR = add([xnew, ynew], piv)
-  return vecR
-}
-
-function setAngle (vec, ang) {
-  let l = mag(vec)
-  let vecR = []
-  vecR[0] = l * Math.cos(ang)
-  vecR[1] = l * Math.sin(ang)
-  return vecR
-}
-
 /**
  * Creates a vector from a magnitud and a direction
  * @param {number} direction This is the direction of the vector that will be created
  * @param {number} magnitud This is the magnitud of the vector that will be created
  */
 function angleMagnitude (direction, magnitud) {
-  let result = [1, 1]
+  let result = setMag([1, 1], magnitud)
   result = setAngle(result, direction)
-  result = setMag(result, magnitud)
   return result
 }
 
+/**
+ * Sets the angle of the given vector to the given angle
+ * @param {array} vector This is the vector that will be changed
+ * @param {number} angle This will be the angle of the given vector in radians
+ */
 function linearIntersect (a, b, c, d) {
   let r = sub(b, a)
   let s = sub(d, c)
@@ -238,7 +227,33 @@ function linearIntersect (a, b, c, d) {
     u = 1
     t = 1
   }
-  return {u, t}
+  return [u, t]
+}
+
+/**
+ * Adds the angle of the given vector to the given angle
+ * @param {array} vector This is the vector that will be changed
+ * @param {number} angle This will be the angle that will be added to the given vector in radians
+ * @param {array} piv This is the center of rotation
+ */
+function addAngle (vec, ang, piv = [0, 0]) {
+  let s = Math.sin(ang)
+  let c = Math.cos(ang)
+  let vecR = sub(vec, piv)
+  let xnew = vecR[0] * c - vecR[1] * s
+  let ynew = vecR[0] * s + vecR[1] * c
+  vecR = add([xnew, ynew], piv)
+  return vecR
+}
+
+/**
+ * Sets the angle of the given vector to the given angle
+ * @param {array} vector This is the vector that will be changed
+ * @param {number} angle This will be the angle of the given vector in radians
+ */
+function setAngle (vec, ang) {
+  let r = mag(vec)
+  return [r * Math.cos(ang), r * Math.sin(ang)]
 }
 
 module.exports = {
